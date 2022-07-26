cff3679a3c158fc2539ba08c43d1e3725c88bfb7
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-07-26 08:39:10 +0600

    first commit -m

---
+++ src/main/java/com/example/oauth2_rolebased_authorization/Oauth2RoleBasedAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_rolebased_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2RoleBasedAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2RoleBasedAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oauth2_rolebased_authorization/Oauth2RoleBasedAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_rolebased_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2RoleBasedAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
